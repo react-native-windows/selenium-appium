3f68734903115738fdce5142304b5ceeb6a66c9f
Canhua Li <licanhua@live.com>
2019-09-18 14:54:22 -0700

    prepare for pipeline

--- example/__tests__/Times.ts
+++ example/__tests__/Times.ts
@@ -5,10 +5,11 @@ describe('Timeout example', () => {
     // There are two global timers: waitforPageTimeout and waitforTimeout
     // waitforTimeout is used by By2. When call any WebElement function of By2, it loops until the element is located
     // waitforPageTimeout is used by PageObject. It defined the default timeout for waitForPageLoaded.
-
+    test("Timer testing", () => {
     Config.setWaitForPageTimeout(100);
     expect(Config.getWaitForPageTimeout()).toBe(100);
 
     Config.setWaitForTimeout(100);
     expect(Config.getWaitForTimeout()).toBe(100);
+    });
 });
