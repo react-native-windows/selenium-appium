166651237aa51132b6bc1b46a583ee664b4e7359
Canhua Li <licanhua@live.com>
2019-09-23 16:21:49 -0700

    lowercase filename bump version

--- src/by2.ts
+++ src/by2.ts
@@ -6,8 +6,8 @@
 'use strict';
 
 import { By, Locator, WebDriver, ISize, IRectangle, ILocation, until, IWebElementId, WebElement, WebElementPromise, ThenableWebDriver, FileDetector, Session, Actions, WebElementCondition, Condition, Options, Navigation, TargetLocator } from 'selenium-webdriver'
-import { driver, WebDriver2 } from './Driver'
-import { Config } from './Config'
+import { driver, WebDriver2 } from './driver'
+import { Config } from './config'
 
 interface IWebElement extends WebElement {
 
